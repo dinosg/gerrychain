2dfb6a6f9accb668a792294c767d7f0e4e82fd34
dinosg <dinosg@users.noreply.github.com>
2020-05-12 14:28:54 -0400

    Add files via upload

--- runningachain_50pc.py
+++ runningachain_50pc.py
@@ -18,41 +18,50 @@ from strcmp_matlab import strfilter
 import time
 from norm_50 import norm_data
 from get_districtlabels import get_labels
-
-
+from get_electioninfo import get_elections
+from gerrychain.tree import recursive_tree_part
 
 #SET CONSTANTS HERE:
-
-my_apportionment = "CD_2011"    #type of district boundaries to calculate - eg US congressional, state senate, house etc.
+#dontfeedin = 1  #if set=0, feeds in data, otherwise skip
+my_apportionment = "CD_2011"  #"CD_2011"    #type of district boundaries to calculate - eg US congressional, state senate, house etc.
 my_electionproxy = "SEN12"           #pick the election to use as a statewide proxy for partisan voting for districted seats
-my_electionproxy_alternate ="USS12"  #alternate name, see 'elections' variable below
+my_electionproxy_alternate ="SEN12"  #alternate name, see 'elections' variable below
+#my_electiondatafile = "./shapefiles_multistate/TX_vtds/TX_vtds_x.shp"  #"./PA-shapefiles-master/PA_VTDs.json"   #PATH to the election data
+#my_electiondatafile  = "./shapefiles_multistate/WI-shapefiles-master/WI_wards_12_16/WI_ltsb_corrected_final.json"
 my_electiondatafile = "./PA-shapefiles-master/PA_VTDs.json"   #PATH to the election data
 markovchainlength = 1000  #length of Markov chain
 proposaltype = "recom"
-
-
-
-if ".json" in my_electiondatafile:
-    graph_PA = Graph.from_json(my_electiondatafile)
+state = "PA"
+poptol = 0.02
+elections = get_elections(state)
+if 'dontfeedin' in globals():
+    if dontfeedin == 0 or not( 'graph_PA' in globals()):
+        if ".json" in my_electiondatafile:
+            graph_PA = Graph.from_json(my_electiondatafile)
+        else:
+            graph_PA = Graph.from_file(my_electiondatafile)
 else:
-    graph_PA = Graph.from_file(my_electiondatafile)
+    print("reading in data:\n")
+    if ".json" in my_electiondatafile:
+        graph_PA = Graph.from_json(my_electiondatafile)
+    else:
+        graph_PA = Graph.from_file(my_electiondatafile)
     
 t0=time.time()
-elections = [
-    Election("SEN10", {"Democratic": "SEN10D", "Republican": "SEN10R"}),
-    Election("SEN12", {"Democratic": "USS12D", "Republican": "USS12R"}),
-    Election("SEN16", {"Democratic": "T16SEND", "Republican": "T16SENR"}),
-    Election("PRES12", {"Democratic": "PRES12D", "Republican": "PRES12R"}),
-    Election("PRES16", {"Democratic": "T16PRESD", "Republican": "T16PRESR"})
-]
-
+if "TOTPOP" in graph_PA._node[0]:
+    popkey = "TOTPOP"
+elif "PERSONS" in graph_PA._node[0]:
+    popkey = "PERSONS"
+else:
+    popkey = []
+    print("woops no popkey in file, look @ graph_PA._node[0] to figure out what the keyword for population is\n")
 #CONFIGURE UPDATERS
 #We want to set up updaters for everything we want to compute for each plan in the ensemble.
 
 
 # Population updater, for computing how close to equality the district
 # populations are. "TOTPOP" is the population column from our shapefile.
-my_updaters = {"population": updaters.Tally("TOTPOP", alias="population")}
+my_updaters = {"population": updaters.Tally(popkey, alias="population")}
 
 # Election updaters, for computing election results using the vote totals
 # from our shapefile.
@@ -61,10 +70,9 @@ my_updaters.update(election_updaters)
 
 
 #INITIAL PARTITION
-initial_partition, graph_PA, my_updaters = norm_data(graph_PA, my_updaters, "CD_2011", my_electionproxy, my_electionproxy_alternate)
-#initial_partition = GeographicPartition(graph_PA, assignment="CD_2011", updaters=my_updaters)
-cds = get_labels(initial_partition, my_electionproxy) #get congressional district labels
 
+#cds = get_labels(initial_partition, my_electionproxy) #get congressional district labels
+initial_partition = GeographicPartition(graph_PA, assignment=my_apportionment, updaters=my_updaters)
 #SETUP MARKOV CHAIN PROPOSAL W RECOM
 # The ReCom proposal needs to know the ideal population for the districts so that
 # we can improve speed by bailing early on unbalanced partitions.
@@ -74,9 +82,9 @@ ideal_population = sum(list(initial_partition["population"].values())) / len(ini
 # We use functools.partial to bind the extra parameters (pop_col, pop_target, epsilon, node_repeats)
 # of the recom proposal.
 proposal = partial(recom,
-                   pop_col="TOTPOP",
+                   pop_col=popkey,
                    pop_target=ideal_population,
-                   epsilon=0.02,
+                   epsilon=poptol,
                    node_repeats=2
                   )
 
@@ -87,8 +95,30 @@ compactness_bound = constraints.UpperBound(
     2*len(initial_partition["cut_edges"])
 )
 
-pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, 0.02)
+pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, poptol)
+nparts = len(initial_partition)
+ranpart = recursive_tree_part(graph_PA, range(nparts), ideal_population, popkey,poptol,node_repeats=1)
+randpartition = GeographicPartition(graph_PA,assignment = ranpart, updaters = my_updaters)
+exec(open("partition_clean.py").read()) 
+chain = MarkovChain(
+    proposal=proposal,
+    constraints=[
+        pop_constraint,
+        compactness_bound
+    ],
+    accept=accept.always_accept,
+    initial_state=initial_partition,
+    total_steps=2
+)
 
+data = pandas.DataFrame(
+    sorted(partition[my_electionproxy].percents("Democratic"))
+    for partition in chain
+)
+t0=time.time()
+#now can do initial_partition and know my_electionproxy will be OK, won't need alternate
+initial_partition, graph_PA, my_updaters = norm_data(graph_PA, my_updaters, my_apportionment, my_electionproxy, my_electionproxy_alternate)
+cds = get_labels(initial_partition, my_electionproxy) #get congressional district labels
 
 #CONFIGURE MARKOV CHAIN
 
@@ -101,9 +131,10 @@ chain = MarkovChain(
     accept=accept.always_accept,
     initial_state=initial_partition,
     total_steps=markovchainlength
+    )
 
 # This will take about 10 minutes.
-cds = get_labels(initial_partition, my_electionproxy)
+
 # This will take about 10 minutes.
 rsw = []
 rmm = []
--- runningachain_plain.py
+++ runningachain_plain.py
@@ -19,28 +19,44 @@ from strcmp_matlab import strfilter
 import time
 from norm_50 import norm_data
 from get_districtlabels import get_labels
+from get_electioninfo import get_elections
+from gerrychain.tree import recursive_tree_part
+import backup_chain as bc
 
 #SET CONSTANTS HERE:
+dontfeedin = 0  #if set=0, feeds in data, otherwise skip
 
-my_apportionment = "CD_2011"    #type of district boundaries to calculate - eg US congressional, state senate, house etc.
-my_electionproxy = "SEN12"           #pick the election to use as a statewide proxy for partisan voting for districted seats
-my_electiondatafile = "./PA-shapefiles-master/PA_VTDs.json"   #PATH to the election data
-markovchainlength = 50000  #length of Markov chain
+markovchainlength = 5000  #length of Markov chain
 proposaltype = "recom"
+#exec(open("input_templates/WI_SEN_SEN16.py").read()) 
+#exec(open("input_templates/PA_CD_2011_SEN12.py").read()) 
 
-if ".json" in my_electiondatafile:
-    graph_PA = Graph.from_json(my_electiondatafile)
+exec(open("input_templates/MI_HDIST_PRES16.py").read()) 
+
+elections = get_elections(state)
+
+poptol = 0.03 # population tolerance
+
+if 'dontfeedin' in globals():
+    if dontfeedin == 0 or not( 'graph_PA' in globals()):
+        if ".json" in my_electiondatafile:
+            graph_PA = Graph.from_json(my_electiondatafile)
+        else:
+            graph_PA = Graph.from_file(my_electiondatafile)
 else:
-    graph_PA = Graph.from_file(my_electiondatafile)
-    
+    if ".json" in my_electiondatafile:
+        graph_PA = Graph.from_json(my_electiondatafile)
+    else:
+        graph_PA = Graph.from_file(my_electiondatafile)
+     
 t0=time.time()
-elections = [
-    Election("SEN10", {"Democratic": "SEN10D", "Republican": "SEN10R"}),
-    Election("SEN12", {"Democratic": "USS12D", "Republican": "USS12R"}),
-    Election("SEN16", {"Democratic": "T16SEND", "Republican": "T16SENR"}),
-    Election("PRES12", {"Democratic": "PRES12D", "Republican": "PRES12R"}),
-    Election("PRES16", {"Democratic": "T16PRESD", "Republican": "T16PRESR"})
-]
+if "TOTPOP" in graph_PA._node[0]:
+    popkey = "TOTPOP"
+elif "PERSONS" in graph_PA._node[0]:
+    popkey = "PERSONS"
+else:
+    popkey = []
+    print("woops no popkey in file, look @ graph_PA._node[0] to figure out what the keyword for population is\n")
 
 #CONFIGURE UPDATERS
 #We want to set up updaters for everything we want to compute for each plan in the ensemble.
@@ -48,7 +64,11 @@ elections = [
 
 # Population updater, for computing how close to equality the district
 # populations are. "TOTPOP" is the population column from our shapefile.
-my_updaters = {"population": updaters.Tally("TOTPOP", alias="population")}
+## for TX and PA:
+#my_updaters = {"population": updaters.Tally("TOTPOP", alias="population")}
+
+##for WI:
+my_updaters = {"population": updaters.Tally(popkey, alias="population")}
 
 # Election updaters, for computing election results using the vote totals
 # from our shapefile.
@@ -59,7 +79,7 @@ my_updaters.update(election_updaters)
 #INITIAL PARTITION
 #initial_partition, graph_PA, my_updaters = norm_data(graph_PA, my_updaters, "CD_2011", "SEN12", "USS12")
 initial_partition = GeographicPartition(graph_PA, assignment=my_apportionment, updaters=my_updaters)
-
+cds = get_labels(initial_partition, my_electionproxy) #get congressional district labels
 
 #SETUP MARKOV CHAIN PROPOSAL W RECOM
 # The ReCom proposal needs to know the ideal population for the districts so that
@@ -69,11 +89,14 @@ ideal_population = sum(list(initial_partition["population"].values())) / len(ini
 
 # We use functools.partial to bind the extra parameters (pop_col, pop_target, epsilon, node_repeats)
 # of the recom proposal.
+#TX & PA:
+
+#WI:
 if "recom" in proposaltype:
     proposal = partial(recom,
-                       pop_col="TOTPOP",
+                       pop_col=popkey,
                        pop_target=ideal_population,
-                       epsilon=0.02,
+                       epsilon=poptol,
                        node_repeats=2
                       )
 
@@ -81,13 +104,16 @@ if "recom" in proposaltype:
     compactness_bound = constraints.UpperBound(
         lambda p: len(p["cut_edges"]),
         2*len(initial_partition["cut_edges"])
-    )
+        )
     
-    pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, 0.02)
-
-
+    pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, poptol)
+    nparts = len(initial_partition)
+    
 #CONFIGURE MARKOV CHAIN
+    ranpart = recursive_tree_part(graph_PA, range(nparts), ideal_population, popkey, poptol,node_repeats=1)
     
+    randpartition = GeographicPartition(graph_PA,assignment = ranpart, updaters = my_updaters)
+   # exec(open("partition_clean.py").read()) 
     chain = MarkovChain(
         proposal=proposal,
         constraints=[
@@ -95,16 +121,18 @@ if "recom" in proposaltype:
             compactness_bound
         ],
         accept=accept.always_accept,
-        initial_state=initial_partition,
+        initial_state= randpartition,        #initial_state=initial_partition,
         total_steps=markovchainlength
     )
 else:  #random flip
-    
+    nparts = len(initial_partition)
+    ranpart = recursive_tree_part(graph_PA, range(nparts), ideal_population, popkey,poptol-0.02,node_repeats=1)
+    randpartition = GeographicPartition(graph_PA,assignment = ranpart, updaters = my_updaters)
     chain = MarkovChain(
         proposal=propose_random_flip,
         constraints=[single_flip_contiguous],
         accept=always_accept,
-        initial_state=initial_partition,
+        initial_state=randpartition,
         total_steps=markovchainlength
     )
 """
@@ -115,7 +143,7 @@ data = pandas.datarame(
 )
 """
 # This will take about 10 minutes.
-cds = get_labels(initial_partition, "SEN12")
+
 # This will take about 10 minutes.
 rsw = []
 rmm = []
@@ -148,8 +176,9 @@ data1.boxplot(ax=ax, positions=range(len(data1.columns)))
 plt.plot(data1.iloc[0], "ro")
 
 # Annotate
-ax.set_title("Comparing the 2011 plan to an ensemble")
-ax.set_ylabel("Democratic vote % (Senate 2012)")
+titlestr = state + " " + my_apportionment + "  x" + str(markovchainlength)
+ax.set_title(titlestr)
+ax.set_ylabel("Democratic vote % " + my_electionproxy)
 ax.set_xlabel("Sorted districts")
 ax.set_ylim(0, 1)
 ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
@@ -161,3 +190,6 @@ plt.show()
 
  
 t1=time.time()
+print((t1 - t0)/60 ," min runtime\n")
+outname = "redist_data/" + state + "_" + my_apportionment + "_" + my_electionproxy + "x" + str(markovchainlength)
+bc.save(outname,data1, reg, rmm, rsw)
