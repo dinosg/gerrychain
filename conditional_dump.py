5cb8f361b1618c423985b9f388285fd47f58b511
dinosg <dinosg@users.noreply.github.com>
2021-06-18 15:41:07 -0400

    Add files via upload

--- conditional_dump.py
+++ conditional_dump.py
@@ -24,9 +24,9 @@ def eg_gt_x(part,hi_eg, state, my_apportionment,my_electionproxy, i1, tagtext=''
         metricinfo = {"eg":efficiency_gap(part.state[my_electionproxy]), 
                       "mm": mean_median(part[my_electionproxy]), "sw": part.state[my_electionproxy].wins("Democratic"),
                       "pp": polsby_popper(part.state)}
-        eg_val = round(efficiency_gap(part.state[my_electionproxy]),3)
+        eg_val = round(efficiency_gap(part.state[my_electionproxy]),4)
         fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
-        my_electionproxy +'_gt_' + str(i1)+ '_' + str(eg_val)+tagtext+'.csv'
+        my_electionproxy +'_gt_' + '_' + str(eg_val)+tagtext + '_' +str(i1)+'.txt'
         dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
     return
                      
@@ -38,9 +38,9 @@ def eg_zero_x(part,zero_eg, state, my_apportionment,my_electionproxy, i1, tagtex
         metricinfo = {"eg":efficiency_gap(part.state[my_electionproxy]), 
                       "mm": mean_median(part.state[my_electionproxy]), "sw": part.state[my_electionproxy].wins("Democratic"),
                       "pp": polsby_popper(part.state)}
-        eg_val = round(efficiency_gap(part.state[my_electionproxy]),3)
+        eg_val = round(efficiency_gap(part.state[my_electionproxy]),4)
         fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
-        my_electionproxy +'_eq_' + str(i1)+ '_' + str(eg_val)+tagtext+'.csv'
+        my_electionproxy +'_eq_' + '_' + str(eg_val)+tagtext + '_' + str(i1)+'.txt'
         dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
     return
             
@@ -50,9 +50,9 @@ def eg_lt_x(part,lo_eg, state, my_apportionment, my_electionproxy,i1, tagtext=''
          metricinfo = {"eg":efficiency_gap(part.state[my_electionproxy]), 
                       "mm": mean_median(part.state[my_electionproxy]), "sw": part.state[my_electionproxy].wins("Democratic"),
                       "pp": polsby_popper(part.state)}
-         eg_val = round(efficiency_gap(part.state[my_electionproxy]),3)
+         eg_val = round(efficiency_gap(part.state[my_electionproxy]),4)
          fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
-         my_electionproxy +'_lt_' + str(i1)+ '_' + str(eg_val)+tagtext+'.csv'
+         my_electionproxy +'_lt_' + '_' + str(eg_val)+tagtext + '_' + str(i1)+'.txt'
          dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
      return
  
@@ -62,9 +62,9 @@ def eg_gt(part,hi_eg, state, my_apportionment,my_electionproxy, i1, tagtext=''):
         metricinfo = {"eg":efficiency_gap(part[my_electionproxy]), 
                       "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
                       "pp": polsby_popper(part)}
-        eg_val = round(efficiency_gap(part[my_electionproxy]),3)
+        eg_val = round(efficiency_gap(part[my_electionproxy]),4)
         fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
-        my_electionproxy +'_gt_' + str(i1)+ '_' + str(eg_val) +tagtext+'.csv'
+        my_electionproxy +'_gt_' + '_' + str(eg_val) +tagtext + '_' + str(i1) +'.txt'
         dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
         #print('blah got here \n')
     return
@@ -77,9 +77,9 @@ def eg_zero(part,zero_eg, state, my_apportionment,my_electionproxy, i1, tagtext=
         metricinfo = {"eg":efficiency_gap(part.state[my_electionproxy]), 
                       "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
                       "pp": polsby_popper(part)}
-        eg_val = round(efficiency_gap(part[my_electionproxy]),3)
+        eg_val = round(efficiency_gap(part[my_electionproxy]),4)
         fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
-        my_electionproxy +'_eq_' + str(i1)+ '_' + str(eg_val)+tagtext+'.csv'
+        my_electionproxy +'_eq_' + '_' + str(eg_val)+tagtext + str(i1)+'.txt'
         dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
     return
             
@@ -89,9 +89,86 @@ def eg_lt(part,lo_eg, state, my_apportionment, my_electionproxy,i1, tagtext=''):
          metricinfo = {"eg":efficiency_gap(part[my_electionproxy]), 
                       "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
                       "pp": polsby_popper(part)}
-         eg_val = round(efficiency_gap(part[my_electionproxy]),3)
+         eg_val = round(efficiency_gap(part[my_electionproxy]),4)
          fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
-         my_electionproxy +'_lt_' + str(i1)+ '_' + str(eg_val)+tagtext+'.csv'
+         my_electionproxy +'_lt_' + '_' + str(eg_val)+tagtext + '_' + str(i1)+'.txt'
          dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
      return
-                       
+ 
+def mm_gt(part,hi_mm, state, my_apportionment,my_electionproxy, i1, tagtext=''):
+    #print('eg_gt ', i1, '\n')
+    if mean_median(part[my_electionproxy]) > hi_mm:
+        metricinfo = {"eg":efficiency_gap(part[my_electionproxy]), 
+                      "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
+                      "pp": polsby_popper(part)}
+        mm_val = round(mean_median(part[my_electionproxy]),4)
+        fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
+        my_electionproxy +'_gt_' + '_' + str(mm_val) +tagtext + '_' + str(i1)+'.txt'
+        dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
+        #print('blah got here \n')
+    return
+                     
+                      
+                      
+    
+def mm_zero(part,zero_mm, state, my_apportionment,my_electionproxy, i1, tagtext=''):
+    if abs(mean_median(part[my_electionproxy])) < zero_mm:
+        metricinfo = {"eg":efficiency_gap(part.state[my_electionproxy]), 
+                      "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
+                      "pp": polsby_popper(part)}
+        mm_val = round(mean_median(part[my_electionproxy]),4)
+        fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
+        my_electionproxy +'_eq_' + '_' + str(mm_val)+tagtext + '_' + str(i1)+'.txt'
+        dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
+    return
+            
+    
+def mm_lt(part,lo_mm, state, my_apportionment, my_electionproxy,i1, tagtext=''):
+     if mean_median(part[my_electionproxy]) < lo_mm:
+         metricinfo = {"eg":efficiency_gap(part[my_electionproxy]), 
+                      "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
+                      "pp": polsby_popper(part)}
+         mm_val = round(mean_median(part[my_electionproxy]),4)
+         fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
+         my_electionproxy +'_lt_' + '_' + str(mm_val)+tagtext + '_' + str(i1)+'.txt'
+         dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
+     return
+     
+def cd_gt(part,hi_lim,the_measure, state, my_apportionment,my_electionproxy, i1, tagtext=''):
+    #print('eg_gt ', i1, '\n')
+    if the_measure > hi_lim:
+        metricinfo = {"eg":efficiency_gap(part[my_electionproxy]), 
+                      "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
+                      "pp": polsby_popper(part)}
+        mm_val = round(the_measure,4)
+        fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
+        my_electionproxy +'_gt_' + '_' + str(mm_val) +tagtext + '_' + str(i1)+'.txt'
+        dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
+        #print('blah got here \n')
+    return
+                     
+                      
+                      
+    
+def cd_zero(part,zero_lim, the_measure, state, my_apportionment,my_electionproxy, i1, tagtext=''):
+    if abs(the_measure) < zero_lim:
+        metricinfo = {"eg":efficiency_gap(part.state[my_electionproxy]), 
+                      "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
+                      "pp": polsby_popper(part)}
+        mm_val = round(the_measure,4)
+        fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
+        my_electionproxy +'_eq_' + '_' + str(mm_val)+tagtext + '_' + str(i1)+'.txt'
+        dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
+    return
+            
+    
+def cd_lt(part,lo_lim, the_measure, state, my_apportionment, my_electionproxy,i1, tagtext=''):
+     if the_measure < lo_lim:
+         metricinfo = {"eg":efficiency_gap(part[my_electionproxy]), 
+                      "mm": mean_median(part[my_electionproxy]), "sw": part[my_electionproxy].wins("Democratic"),
+                      "pp": polsby_popper(part)}
+         mm_val = round(the_measure,4)
+         fname = 'redist_data/example_districts/' + state + '_' + my_apportionment + '_' + \
+         my_electionproxy +'_lt_' + '_' + str(mm_val)+tagtext + '_' + str(i1)+'.txt'
+         dl.part_dump(part, fname, metricinfo) #dump out the district assignment data + GEOID10 tags etc. to file
+     return                  
